595a618fe7564c4d0549602df56058ee617473cc
Alejandro Chanocua <73016752+alexchanocua@users.noreply.github.com>
2021-07-27 18:30:13 -0700

    fixed ssl in connection

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ const db = knex({
     client: 'pg',
     connection: {
       connectionString: process.env.DATABASE_URL, // heroku PSQL
-      ssl: true
+      ssl: {
+        rejectUnauthorized: false
+      }
     }
   });
 
